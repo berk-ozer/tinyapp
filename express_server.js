425ebb92c7457c5e851082c6631a07c978f05203
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-17 20:44:03 +0000

    Add body-parser as a dependency

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
