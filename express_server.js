8bf3606b0bfae8a1cb9e19c0e7acdbb39589ca3f
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-17 19:29:17 +0000

    Add /urls/:shortURL route

--- express_server.js
+++ express_server.js
@@ -26,6 +26,10 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render('urls_show', templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
